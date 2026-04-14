6fe937d05fcb8e5cc453606abc11a7bbd540d5fa
Alhadis <gardnerjohng@gmail.com>
2026-04-14 12:18:27 +1000

    Improve output and modularity of coverage checking

--- libexec/check-coverage.sh
+++ libexec/check-coverage.sh
@@ -1,30 +1,45 @@
 #!/bin/sh
 set -e
-cd ~/Music
 
 # List all media files
 all_files()(
-	find . -type f -not \( -path './Music/*' -or -name SHA256 \) \
+	cd ~/Music
+	find . -type f -not \( -regex "^\./Music/.*" -or -name SHA256 \) \
 	| sed 's/^\.\///' \
 	| sort -n
 )
 
 # List media files that have an entry in the library's checksum database
 covered_files(){
-	sed 's/^.\{64\}  *//' SHA256 | sort -n
+	sed -n '/^[0-9A-Fa-f]\{64\}[[:blank:]]\{1,\}\(.*\)$/ {
+		s//\1/
+		s/^\.\///
+		s/[[:blank:]]*$//
+		p
+	}' ~/Music/SHA256 | sort -n
 }
 
+# Assert that all media files are accounted for in the library's “SHA256” file
+check_coverage(){
+	# Use process substitution if the shell supports it
+	# shellcheck disable=SC3001
+	if eval 'grep -q 3 <(echo 1 2 + n | dc)' 2>/dev/null; then
+		diff=`eval "diff -r -U4 <(covered_files) <(all_files)" || :`
+	else
+		set -- /tmp/covered-files.txt /tmp/all-files.txt
+		covered_files > "$1"
+		all_files     > "$2"
+		diff=`diff -r -U4 "$@" || :`
+		rm -f "$@"
+	fi
+	if test -n "$diff"; then
+		printf '%s\n' "$diff" | format-diff | sed '
+			1 s/ expected/ missing files/
+			2 s/ actual/ unlisted files/
+		'
+		return 1
+	fi
+}
 
-# Use process substitution if the shell supports it
-# shellcheck disable=SC3001
-if eval 'grep -q 3 <(echo 1 2 + n | dc)'; then
-	diff -r -U4 <(covered_files) <(all_files)
-else
-	unset status
-	set -- /tmp/covered-files.txt /tmp/all-files.txt
-	covered_files > "$1"
-	all_files     > "$2"
-	diff -r -U4 "$@" || status=$?
-	rm -f "$@"
-	return ${status:-0}
-fi
+echo 'Affirming coverage of checksum database...'
+check_coverage
