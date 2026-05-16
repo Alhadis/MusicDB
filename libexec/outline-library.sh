360e5070057ac348d40d75c59d57b4349aabf96e
Alhadis <gardnerjohng@gmail.com>
2026-05-16 20:50:22 +1000

    Add option to omit access/change times in outlines
    The former is kinda meaningless when determining the parity of filesets,
    whilst the latter isn't even supported by all filesystems (not that it's
    even possible to do anything about a file's ctime, anyway).

--- libexec/outline-library.sh
+++ libexec/outline-library.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
+
+#
+# outline-library.sh: Generate diff(1)-friendly outline of directory
+#
 set -e
 
-# Generate diff(1)-friendly outline of library
-print_outline(){
-	cd ~/Music
-	find . \( -type f -or -type d \) -mindepth 1 -not \( \
-		-path './Music'   -or \
-		-path './Music/*' -or \
-		-path './SHA256' \
-	\) -exec stat -f '%Fa %Fm %Fc %Sp %010z%t%N' {} + \
-	| LANG=C sort -k6 | format_outline "$@"
-}
+
+# Omit last-access and last-changed timestamps if “-m” switch is passed.
+# (NB: This must be the first argument provided).
+export m=0 >/dev/null 2>&1 || :
+case $1 in
+	-m?*) head="${1#??}"; m=1; shift; set -- "-$head" "$@";;
+	-m|--mtime-only) m=1; shift;;
+esac
+
 
 # Wrangle the printed outline into a desired format
 format_outline(){
@@ -28,27 +31,39 @@ format_outline(){
 				if(name ~ /^\.\//)     name = substr(name, 3)
 				if(match(size, /^0+/)) size = substr(size, RLENGTH + 1)
 				print name
-				printf "\tMode:     %s\n", mode
-				printf "\tAccessed: %s\n", atime
-				printf "\tModified: %s\n", mtime
-				printf "\tChanged:  %s\n", ctime
+				if('"$m"'){
+					printf "\tMode:     %s\n", mode
+					printf "\tModified: %s\n", mtime
+				}
+				else{
+					printf "\tMode:     %s\n", mode
+					printf "\tAccessed: %s\n", atime
+					printf "\tModified: %s\n", mtime
+					printf "\tChanged:  %s\n", ctime
+				}
 				if(mode !~ /^d/) printf "\tFilesize: %s\n", size
 			}
 		';;
 		# JSON-formatted object array
 		-j|--json)
 			echo "["
-			sed '
-				s/"/\\"/g
+			if [ "$m" = 1 ]; then m='
+				s/^/{"mtime":"/
+			'; else m='
 				s/^/{"atime":"/
 				s/ /","mtime":"/
 				s/ /","ctime":"/
+			'; fi
+			set -x
+			sed '
+				s/"/\\"/g'"$m"'
 				s/ /","mode":"/
 				s/ 0*/","size":/
 				s/	\(\.\/\)\{0,1\}/,"name":"/
 				s/[[:blank:]]*$/"}/
 				$! s/$/,/
 			'
+			set +x
 			echo "]"
 		;;
 		# Strip leading “./” from filename field
@@ -56,4 +71,14 @@ format_outline(){
 	esac
 }
 
-print_outline "$@"
+case $m in
+	1) key=4; stat='%Fm %Sp %010z%t%N';;
+	*) key=6; stat='%Fa %Fm %Fc %Sp %010z%t%N';;
+esac
+
+find . \( -type f -or -type d \) -mindepth 1 -not \( \
+	-path './Music'   -or \
+	-path './Music/*' -or \
+	-path './SHA256' \
+\) -exec stat -f "$stat" {} + \
+| LANG=C sort "-k$key" | format_outline "$@"
